abce3716c281eaf504a8702e9bfac587beeb00e3
Marvin Daniel Rodriguez <mdanielcool98@gmail.com>
2022-08-14 01:37:58 -0600

    host externo

--- client/src/features/vehicles/vehicleSlice.js
+++ client/src/features/vehicles/vehicleSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import Swal from "sweetalert2";
 
-const BACK_URL = "http://backend:8080";
+const BACK_URL = "http://192.168.122.170:8080";
 
 const vehicleSlice = createSlice({
   name: "vehicles",
